5bae7dd408a0a2aab8527ed6b6a9a2361d025da4
xur <645531705@qq.com>
2016-10-20 00:01:25 +0800

    201610200001

--- app/src/main/java/com/example/xur/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/example/xur/sharedpreferencestest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.xur.sharedpreferencestest;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age",28);
                 editor.putBoolean("married",false);
                 editor.commit();
+            }
+        });
+        Button restoreData=(Button)findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences=getSharedPreferences("data",MODE_PRIVATE);
+                String name=preferences.getString("name","");
+                int age=preferences.getInt("age",0);
+                boolean married=preferences.getBoolean("married",false);
+                Log.d("MainActivity","name is"+name);
+                Log.d("MainActivity","age is"+age);
+                Log.d("MainActivity","married is"+married);
             }
         });
     }
